0995e138ce8658601703ec868c1282edd6676bf2
Abhinav <codedreamer06@gmail.com>
2022-02-02 13:41:36 +0530

    Basic undo feature

--- Program.cs
+++ Program.cs
@@ -11,7 +11,18 @@ namespace LogSuite
 2 - Add a new transaction
 3 - Remove a transaction
 4 - Edit a transaction
+5 - Undo your previous action
 0 - Go back";
+
+        enum DbActionType
+        {
+            Create,
+            Update,
+            Delete
+        }
+
+        private static Transaction currentTransaction;
+        private static DbActionType currentActionType;
 
         private static void Main()
         {
@@ -22,7 +33,8 @@ namespace LogSuite
               { 1, Read },
               { 2, Create },
               { 3, Delete },
-              { 4, Update }
+              { 4, Update },
+              { 5, Undo }
             };
 
             while (true)
@@ -50,6 +62,43 @@ namespace LogSuite
             }
         }
 
+        private static void Undo()
+        {
+            if (currentTransaction == null) return;
+
+            using (var db = new TransactionContext())
+            {
+                try
+                {
+                    if (currentActionType == DbActionType.Create)
+                    {
+                        db.Remove(currentTransaction);
+                        db.SaveChanges();
+                    }
+
+                    else if (currentActionType == DbActionType.Update)
+                    {
+                        var record = db.transactions?.Where(t => t.Id == currentTransaction.Id).First();
+                        record = currentTransaction;
+                        db.SaveChanges();
+                    }
+
+                    else if (currentActionType == DbActionType.Delete)
+                    {
+                        db.Add(currentTransaction);
+                        db.SaveChanges();
+                    }
+
+                    Console.WriteLine("Your last action was undone.");
+                }    
+                
+                catch
+                {
+                    Console.WriteLine("Sorry, couldn't undo your last action.");
+                }
+            }            
+        }
+
         private static void Create()
         {
             using (var db = new TransactionContext())
@@ -57,8 +106,15 @@ namespace LogSuite
                 try
                 {
                     var transactionData = AcceptInput("Customer Name", "Total Price");
-                    db.Add(new Transaction { CustomerName = transactionData[0], TotalPrice = float.Parse(transactionData[1]) });
+                    currentTransaction = new Transaction { 
+                        CustomerName = transactionData[0],
+                        TotalPrice = float.Parse(transactionData[1]) 
+                    };
+
+                    db.Add(currentTransaction);
                     db.SaveChanges();
+
+                    currentActionType = DbActionType.Create;
                     Console.WriteLine($"Successfuly added {transactionData[0]} with a bill of {transactionData[1]}");
                 }
 
@@ -92,8 +148,14 @@ namespace LogSuite
                     var transactionData = AcceptInput("id", "new price");
                     var absoluteId = transactions[Convert.ToInt32(transactionData[0]) - 1].Id;
                     var record = db.transactions!.Where(t => t.Id == absoluteId).First();
+
+                    currentActionType = DbActionType.Update;
+                    currentTransaction = record;
+
                     record.TotalPrice = Convert.ToInt32(transactionData[1]);
+
                     db.SaveChanges();
+
                     Console.WriteLine($"{record.CustomerName}'s bill has been changed to {record.TotalPrice} successfully");
                 }
 
@@ -105,7 +167,7 @@ namespace LogSuite
         }
 
         private static void Delete()
-        {
+        { 
             using (var db = new TransactionContext())
             {
                 var transactions = db.transactions!.OrderBy(t => t.Id).ToList();
@@ -114,8 +176,13 @@ namespace LogSuite
                     var relativeId = AcceptInput("id")[0];
                     var absoluteId = transactions[Convert.ToInt32(relativeId) - 1].Id;
                     var record = db.transactions!.Where(t => t.Id == absoluteId).First();
+
+                    currentActionType = DbActionType.Delete;
+                    currentTransaction = record;
+
                     db.Remove(record);
                     db.SaveChanges();
+
                     Console.WriteLine($"Successfully deleted the transaction of {record.CustomerName}");
                 }
 
